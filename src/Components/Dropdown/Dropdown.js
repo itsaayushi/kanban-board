ab371d044677ad61859b8c6867299aeb4997a1b0
Aayushi <itsaayushiemail@gmail.com>
2023-05-12 21:37:49 +0530

    first commit

---
+++ src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from "react";
+function Dropdown(props) {
+  const dropdownRef = useRef();
+
+  const handleClick = (event) => {
+    if (dropdownRef && !dropdownRef?.current?.contains(event?.target)) {
+      if (props.onClose) props.onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", handleClick, { capture: true });
+    };
+  });
+
+  return (
+    <div
+      ref={dropdownRef}
+      className="dropdown"
+      style={{ position: "absolute", top: "100", right: "0" }}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Dropdown;
